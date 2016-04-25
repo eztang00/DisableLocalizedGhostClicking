69629f51e49122f3b95cef6ceb33172e3a264173
eztang00 <eztang00@gmail.com>
2016-04-25 14:41:52 -0700

    change comments

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
+echo You may close this window
 cd DisableLocalizedGhostClicking\bin && javaw DisableLocalizedGhostClicking
-::scroll right to edit comment
-echo                                                                                                    (I'm assuming you're using windows) If javaw.exe not found even though you installed Java and have a javaw.exe file, go to the Control Panel, type "System" in the search box, click "Edit the system environment variables", "Environment Variables...", scroll down, click "Path", "Edit...", and type where javaw.exe is located                                                                                                    
+echo OFF
+echo (I'm assuming you're using windows) If it says "'javaw' is not recognized", do the following. Double check you installed Java. Go to the Control Panel, type "System" in the search box, click "Edit the system environment variables", "Environment Variables...", scroll until you see "Path", select it, click "Edit...", click the leftmost of the Variable value textfield, (make sure it's NOT highlighted) type javaw.exe is, and then type a ";" (EXAMPLE: "C:\Program Files\Java\jre1.8.0_45\bin;" yours will differ from the example)
 pause
